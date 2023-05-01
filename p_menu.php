834756225a2bbe24e7dccaeb71328f48dfee4038
CasandraUmblas <21-00506@g.batstate-u.edu.ph>
2023-05-01 20:48:49 +0800

    p_menu

---
+++ p_menu.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" contents="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <form action="" method="post">
+            <div class="field input mb-3 col-6 mx-auto">
+                <div class="text-center mb-4">
+                    <h3>Patient Menu</h3>
+                </div>
+                <a href="p_add.php" class="btn btn-dark mb-3">Create an account</a>
+                <a href="med_search.php" class="btn btn-dark mb-3">Search medicine</a>
+                <a href="med_update.php" class="btn btn-dark mb-3">Update/Monitor</a>
+                <a href="med_update.php" class="btn btn-dark mb-3">Delete</a>
+            </div>
+
+            <div>
+                    <a href="" class="btn btn-danger">Back</a>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</body>
+</html>
