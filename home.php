8c6734cf5c7c6545729e3c47bab7803f4fd84df6
CasandraUmblas <21-00506@g.batstate-u.edu.ph>
2023-05-13 12:13:45 +0800

    update

--- home.php
+++ home.php
@@ -1,44 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pharmacy Management System</title>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
-    <style>
-        *{
-            font-family: 'Poppins', sans-serif;
-        }
-        .h-font{
-            font-family: 'Merienda', cursive;
-        }
-
-    </style>
+	<title>BIOTECH PHARMACY</title>
+	<link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
-        <div class="container-fluid">
-            <a class="navbar-brand">Biotech Pharmacy</a>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="abt_us.html">About</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="cont_us.html">Contact Us</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+	<header>
+		<div class="main">
+			<div class="logo">
+				<img src="LOGO.png">
+			<ul> 
+				<li> <a href="home.php"> Home </a> </li>
+				<li> <a href="cont_us.php"> Contact </a> </li>
+			</ul>
+			</div>
+		</div>
+		
+		<div class="title">
+			<h1>BIOTECH PHARMACY</h1>
+		</div>
+	</header>
 </body>
+</html>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title> BIOTECH PHARMACY</title>
+	</head>
+	<body>
+		<a href="https://www.google.com">
+			<button onclick="doSomething()" style="background-color:A0D8B3;color:#263A29;border-radius:5px">click me!</button>
+		</a>
+
+		<p id="test">Hello</p>
+
+		<script>
+			function doSomething(){
+			document.getElementById("test").innerHTML = "Goodbye";
+			}
+		</script>
+	</body>
 </html>
